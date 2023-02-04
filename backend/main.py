ea06f3b9d30a4d93fa76319865e3fc665877157d
Xin Wang <xin.wang19@imperial.ac.uk>
2023-02-04 22:29:48 +0000

    influxDB client created

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from influxDB import InfluxDbManager
+from influxdb_client.client.write_api import SYNCHRONOUS, WriteOptions
+
+BUCKET_NAME="patientData"
+MEASUREMENT_NAME=""
+
+influxdb_client = InfluxDbManager()
+
+def create_application() -> FastAPI:
+
+    app = FastAPI(
+        title="Canary backend services"
+    )
+    # Configuring CORS policies
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    # Connecting to database
+    influxdb_client.check_connection()
+
+    return app
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+session = create_application()
